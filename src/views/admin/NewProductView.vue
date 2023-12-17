f5df1ee3c3496b6a55d3ae61ad72e854ecfacc10
Daniel151505 <daniel150520011@gmail.com>
2023-12-17 00:08:24 -0500

    feat: Added CSS to the submit button

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -8,55 +8,59 @@ import Link from "@/components/Link.vue";
 
     <h1 class="text-4xl font-black my-10">New Product</h1>
 
-    <div class="flex justify-center bg-white shadow"></div>
+    <div class="flex justify-center bg-white shadow">
+      <div class="mt-10 p-10 w-full 2xl:w-2/4">
+        <FormKit type="form" submit-label="Add Product">
+          <FormKit
+            type="text"
+            label="Name"
+            name="name"
+            placeholder="Product Name"
+            validation="required"
+            :validation-messages="{ required: 'Product name is required' }"
+          />
 
-    <FormKit type="form" submit-label="Add Product">
-      <FormKit
-        type="text"
-        label="Name"
-        name="name"
-        placeholder="Product Name"
-        validation="required"
-        :validation-messages="{required: 'Product name is required'}"
-      />
+          <FormKit
+            type="file"
+            label="Product Image"
+            name="image"
+            validation="required"
+            :validation-messages="{ required: 'Product image is required' }"
+            accept=".jpg"
+          />
 
-      <FormKit
-        type="file"
-        label="Product Image"
-        name="image"
-        validation="required"
-        :validation-messages="{required: 'Product image is required'}"
-        accept=".jpg"
-      />
+          <FormKit
+            type="select"
+            label="Category"
+            name="category"
+            validation="required"
+            :validation-messages="{ required: 'Category is required' }"
+            :options="[1, 2, 3]"
+          />
 
-      <FormKit
-        type="select"
-        label="Category"
-        name="category"
-        validation="required"
-        :validation-messages="{required: 'Category is required'}"
-        :options="[1,2,3]"
-      />
+          <FormKit
+            type="number"
+            label="Price"
+            name="price"
+            placeholder="Product Price"
+            validation="required"
+            :validation-messages="{ required: 'Product Price is required' }"
+            min="1"
+          />
 
-      <FormKit
-        type="number"
-        label="Price"
-        name="price"
-        placeholder="Product Price"
-        validation="required"
-        :validation-messages="{required: 'Product Price is required'}"
-        min="1"
-      />
-
-      <FormKit
-        type="number"
-        label="Available"
-        name="availability"
-        placeholder="Quantity Available"
-        validation="required"
-        :validation-messages="{required: 'Quantity Available is required'}"
-        min="1"
-      />
-    </FormKit>
+          <FormKit
+            type="number"
+            label="Available"
+            name="availability"
+            placeholder="Quantity Available"
+            validation="required"
+            :validation-messages="{
+              required: 'Quantity Available is required',
+            }"
+            min="1"
+          />
+        </FormKit>
+      </div>
+    </div>
   </div>
 </template>
