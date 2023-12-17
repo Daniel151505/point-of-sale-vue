6a199fdc87c58b4a29a5a2c83c3425e94d3ec24b
Daniel151505 <daniel150520011@gmail.com>
2023-12-17 16:29:16 -0500

    feat: Added upload images to Firebase Storage

--- src/composables/useImage.js
+++ src/composables/useImage.js
@@ -1,6 +1,35 @@
+import { uid } from "uid";
+import { useFirebaseStorage } from "vuefire";
+import {
+  ref as StorageRef,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/database";
+
 export default function useImage() {
+  const storage = useFirebaseStorage();
+
   const onFileChange = (e) => {
-    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const filename = uid() + ".jpg";
+    const sRef = StorageRef(storage, "/products/" + filename);
+
+    // Upload file
+    const uploadTask = uploadBytesResumable(sRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      () => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        // Upload is complete
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log(downloadURL);
+        });
+      }
+    );
   };
 
   return {
