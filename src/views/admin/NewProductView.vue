2d90e89a6f6612315601f21a583335a120b2dbc6
Daniel151505 <daniel150520011@gmail.com>
2023-12-16 23:23:37 -0500

    feat: Added fields in new product

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -8,8 +8,55 @@ import Link from "@/components/Link.vue";
 
     <h1 class="text-4xl font-black my-10">New Product</h1>
 
-    <FormKit type="form">
-      <FormKit type="text" />
+    <div class="flex justify-center bg-white shadow"></div>
+
+    <FormKit type="form" submit-label="Add Product">
+      <FormKit
+        type="text"
+        label="Name"
+        name="name"
+        placeholder="Product Name"
+        validation="required"
+        :validation-messages="{required: 'Product name is required'}"
+      />
+
+      <FormKit
+        type="file"
+        label="Product Image"
+        name="image"
+        validation="required"
+        :validation-messages="{required: 'Product image is required'}"
+        accept=".jpg"
+      />
+
+      <FormKit
+        type="select"
+        label="Category"
+        name="category"
+        validation="required"
+        :validation-messages="{required: 'Category is required'}"
+        :options="[1,2,3]"
+      />
+
+      <FormKit
+        type="number"
+        label="Price"
+        name="price"
+        placeholder="Product Price"
+        validation="required"
+        :validation-messages="{required: 'Product Price is required'}"
+        min="1"
+      />
+
+      <FormKit
+        type="number"
+        label="Available"
+        name="availability"
+        placeholder="Quantity Available"
+        validation="required"
+        :validation-messages="{required: 'Quantity Available is required'}"
+        min="1"
+      />
     </FormKit>
   </div>
 </template>
