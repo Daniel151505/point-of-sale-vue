7742a49cfe93cb19d9a5687b84558eaa7bd30225
Daniel151505 <daniel150520011@gmail.com>
2023-12-19 13:13:39 -0500

    feat: Updated the records

--- src/views/admin/EditProductView.vue
+++ src/views/admin/EditProductView.vue
@@ -34,7 +34,8 @@ watch(product, (product) => {
 
 const submitHandler = async (data) => {
   try {
-    await products.updateProduct(docRef, data);
+    await products.updateProduct(docRef,{...data, url});
+    router.push({ name: "products" });
   } catch (error) {
     console.log(error);
   }
