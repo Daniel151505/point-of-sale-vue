a06a202ae8ca17343b6b2bcf6fadf030dc8eee0a
Daniel151505 <daniel150520011@gmail.com>
2023-12-18 20:35:54 -0500

    feat: Added functionality to fetch products from Firestore.

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,7 +1,7 @@
 import { computed } from "vue";
 import { defineStore } from "pinia";
-import { useFirestore } from "vuefire";
-import { collection, addDoc } from "firebase/firestore";
+import { useFirestore, useCollection } from "vuefire";
+import { collection, addDoc, where, query, limit, orderBy } from "firebase/firestore";
 
 export const useProductsStore = defineStore("products", () => {
   const db = useFirestore();
@@ -11,6 +11,12 @@ export const useProductsStore = defineStore("products", () => {
     { id: 2, name: "Sneackers" },
     { id: 1, name: "Glasses" },
   ];
+
+  const q = query(
+    collection(db, 'products')
+  )
+
+  const productsCollection = useCollection(q)
 
   async function createProduct(product) {
     await addDoc(collection(db, "products"), product);
@@ -29,6 +35,7 @@ export const useProductsStore = defineStore("products", () => {
 
   return {
     createProduct,
+    productsCollection,
     categoryOptions,
   };
 });
--- src/views/admin/ProductsView.vue
+++ src/views/admin/ProductsView.vue
@@ -1,5 +1,9 @@
 <script setup>
 import LinkVue from "@/components/Link.vue";
+import ProductVue from "@/components/Product.vue";
+import { useProductsStore } from "@/stores/products";
+
+const products = useProductsStore();
 </script>
 
 <template>
@@ -7,5 +11,16 @@ import LinkVue from "@/components/Link.vue";
     <LinkVue to="new-product">New Product</LinkVue>
 
     <h1 class="text-4xl font-black my-10">Products</h1>
+
+    <ul
+      role="list"
+      class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-5"
+    >
+      <ProductVue
+        v-for="product in products.productsCollection"
+        :key="product.id"
+        :product="product"
+      />
+    </ul>
   </div>
 </template>
