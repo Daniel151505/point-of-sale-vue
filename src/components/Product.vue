297dceb3447207c0e0505fe857c40c5a6e6eb097
Daniel151505 <daniel150520011@gmail.com>
2023-12-18 20:58:50 -0500

    feat: Products added and displayed

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { formatCurrency } from "../helpers";
 defineProps({
   product: {
     type: Object,
@@ -7,7 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <div>
-    <h1>{{ product.name }}</h1>
-  </div>
+  <li
+    class="flex items-center space-x-6 border border-gray-200 p-6 bg-white shadow"
+  >
+    <img :src="product.image" :alt="product.name" class="h-24 w-24" />
+    <div class="space-y-2">
+      <h3 class="text-gray-900">{{ product.name }}</h3>
+      <p class="font-extrabold">{{ formatCurrency(product.price) }}</p>
+      <p>{{ product.availability }} is available</p>
+    </div>
+  </li>
 </template>
