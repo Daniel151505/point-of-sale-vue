ea13cd70faf4d47aa8b90e4ed788a55753b0f194
Daniel151505 <daniel150520011@gmail.com>
2023-12-19 20:27:42 -0500

    feat: Seeding to the database

---
+++ src/data/products.js
@@ -0,0 +1,243 @@
+export const products = [
+    {
+      "name": "Gray Sweatshirt",
+      "price": 49,
+      "availability": 5,
+      "category": 1
+    },
+    {
+      "name": "Orange Men's Sweatshirt",
+      "price": 29,
+      "availability": 3,
+      "category": 1
+    },
+    {
+      "name": "Blue Sweatshirt",
+      "price": 49,
+      "availability": 8,
+      "category": 1
+    },
+    {
+      "name": "White Sweatshirt",
+      "price": 29,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Black / Orange Sweatshirt",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Black / Red Sweatshirt",
+      "price": 39,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Red Sweatshirt",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "White / Blue Sweatshirt",
+      "price": 49,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Yellow Women's Sweatshirt",
+      "price": 39,
+      "availability": 10,
+      "category": 1
+    },
+    {
+      "name": "Women's Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Casual Shoes",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Pink Women's Sneakers",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray Sneakers",
+      "price": 79,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Red / Black Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Women's Sports Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Blue Men's Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Blue Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Casual Men's Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Casual Gray Sneakers",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Light Boots Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Black Boots Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Black / Blue Boots Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Casual Black Sneakers",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray / Black Casual Sneakers",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Black Men's Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray Men's Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray Men's Casual Shoes",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray / Red Sneakers",
+      "price": 49,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Women's Black Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Blue Casual Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Gray Sports Sneakers",
+      "price": 69,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Green Casual Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Red Casual Sneakers",
+      "price": 59,
+      "availability": 10,
+      "category": 2
+    },
+    {
+      "name": "Sun Glasses - Gray",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Sun Glasses - Black",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Dark Sunglasses",
+      "price": 29,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Sun Glasses",
+      "price": 49,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Blue Finish Sun Glasses",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Modern Black Glasses",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    },
+    {
+      "name": "Classic Glasses",
+      "price": 39,
+      "availability": 10,
+      "category": 3
+    }
+  ];
+  
