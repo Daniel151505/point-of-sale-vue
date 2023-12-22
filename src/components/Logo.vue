39c0b78abee657987a2992699b0634f8e92dfa3f
Daniel151505 <daniel150520011@gmail.com>
2023-12-22 01:28:25 -0500

    feat: Added icon

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
         <h1 class="text-xl sm:text-2xl md:text-3xl font-black text-white">
-            POS <span class="text-green-400">Vue.js</span>
+            POS <span class="text-green-400">SHOP STORE</span>
         </h1>
 </template>
