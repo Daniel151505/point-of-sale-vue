2db6aabf2d73d595b6956d65b2c40bc10bbad16e
Daniel151505 <daniel150520011@gmail.com>
2023-12-21 23:48:04 -0500

    feat: Added validation if the user selected a date

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref } from "vue";
 import VueTailwindDatePicker from "vue-tailwind-datepicker";
+import { useSalesStore } from "../../stores/sales";
 
-const date = red("");
+const sales = useSalesStore();
 
 const formatter = ref({
   date: "DD/MM/YYYY",
@@ -16,13 +17,22 @@ const formatter = ref({
 
     <div class="md:flex md:items-start gap-5">
       <div class="md:w-1/2 lg:w-1/3 bg-white flex justify-center p-5">
-        <VueTailwindDatePicker as-single no-input v-model="date" />
+        <VueTailwindDatePicker
+          as-single
+          no-input
+          v-model="date"
+          :formatter="formatter"
+        />
       </div>
 
       <div
         class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32"
       >
-        <p>Sales here</p>
+        <p class="text-center text-lg" v-if="sales.isDateSelected">
+          Sales of the date: <span class="font-black">{{ sales.date }}</span>
+        </p>
+
+        <p v-else class="text-center text-lg">Select a date</p>
       </div>
     </div>
   </div>
